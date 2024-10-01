dd4c2225269ed8f95debcb95e6b637f153a302bc
yurannhassengo <yurannhassengo@gmail.com>
2024-10-01 16:03:24 -0700

    update AboutUs

--- app/(pages)/About/page.tsx
+++ app/(pages)/About/page.tsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
     <div>
-      
-    </div>
-  )
-}
+      <h1>Sobre Nós</h1>
 
-export default About
+      <h2>Visão</h2>
+      <p>
+        Ser a referência em fornecimento de materiais elétricos em Moçambique, reconhecida pela qualidade dos nossos produtos, pela eficiência dos nossos serviços e pela excelência no atendimento ao cliente.
+      </p>
+
+      <h2>Missão</h2>
+      <p>
+        Nossa missão é fornecer soluções elétricas inovadoras e de alta qualidade, garantindo a satisfação de nossos clientes através de produtos confiáveis e um atendimento excepcional.
+      </p>
+
+      <h2>Valores</h2>
+      <ul>
+        <li><strong>Qualidade:</strong> Compromisso com a excelência em todos os produtos e serviços oferecidos.</li>
+        <li><strong>Inovação:</strong> Busca constante por soluções inovadoras que atendam às necessidades do mercado.</li>
+        <li><strong>Integridade:</strong> Transparência e ética em todas as nossas relações comerciais.</li>
+        <li><strong>Compromisso:</strong> Foco no cliente e em suas necessidades, garantindo a melhor experiência possível.</li>
+      </ul>
+
+      <h2>Nossa Equipe</h2>
+      <p>
+        A <strong>Orn Eletróferragem</strong> é composta por engenheiros e técnicos experientes, prontos para auxiliar você em todas as etapas do seu projeto. Nossa experiência e conhecimento garantem que você receba o suporte necessário para alcançar seus objetivos.
+      </p>
+
+      <h2>Produtos de Qualidade</h2>
+      <p>
+        Oferecemos uma seleção de aços de alta resistência e durabilidade, provenientes das melhores marcas do mercado. Com a nossa linha de produtos, você pode ter a certeza de que está adquirindo qualidade e confiabilidade.
+      </p>
+
+      <h2>Atendimento Personalizado</h2>
+      <p>
+        Na Orn Eletróferragem, priorizamos o atendimento personalizado. Nossas soluções são sob medida para atender às suas necessidades específicas, garantindo que você tenha a melhor experiência possível.
+      </p>
+
+      <h2>Logística Eficiente</h2>
+      <p>
+        Com uma logística eficiente, garantimos a entrega rápida e segura dos materiais em todo o país. Nossa estrutura logística é projetada para atender com agilidade, para que você nunca precise se preocupar com atrasos.
+      </p>
+    </div>
+  );
+};
+
+export default About;
