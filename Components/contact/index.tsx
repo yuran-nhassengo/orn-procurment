4850492e01abadc46e84d54d51a04189f4caa8bd
Djaysambo <vicentesambo3@gmail.com>
2024-11-13 23:24:41 +0200

    update info

--- Components/contact/index.tsx
+++ Components/contact/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Contact = () => {
   return (
-    <section className="bg-blue-600 text-white py-12 px-4 md:px-8 lg:px-16">
+    <section className="bg-blue-600  py-12 px-4 md:px-8 lg:px-16">
       <div className="max-w-4xl mx-auto text-center">
         <h2 className="text-3xl md:text-4xl font-bold mb-6">Entre em Contato Conosco</h2>
         <p className="mb-8 text-base md:text-lg">
@@ -10,24 +10,24 @@ const Contact = () => {
         </p>
         
         <form className="space-y-6">
-          <div>
+          <div className='mb-4'>
             <input 
               type="text" 
               placeholder="Nome Completo" 
-              className="w-full p-3 rounded-lg text-gray-900"
+              className="w-full p-3 rounded-lg text-black s"
             />
           </div>
-          <div>
+          <div className='mb-4'>
             <input 
               type="email" 
               placeholder="Email" 
-              className="w-full p-3 rounded-lg text-gray-900"
+              className="w-full p-3 rounded-lg"
             />
           </div>
-          <div>
+          <div className='mb-4'>
             <textarea 
               placeholder="Mensagem" 
-              className="w-full p-3 rounded-lg text-gray-900" 
+              className="w-full p-3 rounded-lg " 
               rows={5}
             ></textarea>
           </div>
