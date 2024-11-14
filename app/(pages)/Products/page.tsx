1957f2b438af03542a133b6be2a828635d13e3ae
Badrú <sbsbadroo@gmail.com>
2024-11-14 19:23:48 +0200

    update:produtos

--- app/(pages)/Products/page.tsx
+++ app/(pages)/Products/page.tsx
@@ -1,27 +1,49 @@
 "use client";
 import * as React from "react";
 
+const marcas = [
+  {
+    nome: "Todas",
+    imagem: "https://www.valper.com.br/img/parceiros2.jpg",
+  },
+  {
+    nome: "Marca A",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWuRewoTL6pBRR65WtpUvf8nZ256Me2wd5oQ&s",
+  },
+  {
+    nome: "Marca B",
+    imagem: "https://via.placeholder.com/200x100?text=Marca+B",
+  },
+  {
+    nome: "Marca C",
+    imagem: "https://via.placeholder.com/200x100?text=Marca+C",
+  },
+];
+
 const produtosEletronicos = [
   {
     nome: "Resistor 10k Ohm",
     descricao: "Resistor de alta qualidade para circuitos eletrônicos.",
-    preco: "R$ 1,99",
-    imagem: "https://www.pcbmay.com/wp-content/uploads/2023/12/Figure-1-10K-resistor.jpg",
+    imagem:
+      "https://www.pcbmay.com/wp-content/uploads/2023/12/Figure-1-10K-resistor.jpg",
     link: "#detalhes-produto-1",
+    marca: "Marca A",
   },
   {
     nome: "Capacitor 100µF",
     descricao: "Capacitor de alumínio para estabilização de tensão.",
-    preco: "R$ 3,50",
-    imagem: "https://www.alligator-electricals.co.ke/wp-content/uploads/2017/12/IMG-20171207-WA0016.jpg",
+    imagem:
+      "https://www.alligator-electricals.co.ke/wp-content/uploads/2017/12/IMG-20171207-WA0016.jpg",
     link: "#detalhes-produto-2",
+    marca: "Marca B",
   },
   {
     nome: "Transistor NPN",
     descricao: "Transistor para amplificação de sinais elétricos.",
-    preco: "R$ 2,30",
-    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDUVslFtUnmA5-rrcOFfpQtwaPE-iK2CXZLQ&s",
+    imagem:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDUVslFtUnmA5-rrcOFfpQtwaPE-iK2CXZLQ&s",
     link: "#detalhes-produto-3",
+    marca: "Marca C",
   },
 ];
 
@@ -29,7 +51,6 @@ const servicosMontagem = [
   {
     nome: "Montagem de Painel Elétrico",
     descricao: "Montagem de painel elétrico com componentes de alta qualidade.",
-    preco: "R$ 150,00",
     imagem: "https://via.placeholder.com/150",
     link: "#detalhes-servico-1",
   },
@@ -37,7 +58,6 @@ const servicosMontagem = [
     nome: "Instalação de Fios e Cabos",
     descricao:
       "Instalação de fios e cabos para circuitos elétricos residenciais e comerciais.",
-    preco: "R$ 50,00",
     imagem: "https://via.placeholder.com/150",
     link: "#detalhes-servico-2",
   },
@@ -48,7 +68,6 @@ const consultoria = [
     nome: "Consultoria em Projetos Elétricos",
     descricao:
       "Consultoria especializada para desenvolver seu projeto elétrico.",
-    preco: "R$ 200,00",
     imagem: "https://via.placeholder.com/150",
     link: "#detalhes-consultoria-1",
   },
@@ -56,15 +75,20 @@ const consultoria = [
     nome: "Análise de Consumo Energético",
     descricao:
       "Análise detalhada do consumo energético para otimizar sua instalação.",
-    preco: "R$ 250,00",
     imagem: "https://via.placeholder.com/150",
     link: "#detalhes-consultoria-2",
   },
 ];
 
 const Products = () => {
+  const [marcaSelecionada, setMarcaSelecionada] = React.useState("Todas");
   const [modalAberto, setModalAberto] = React.useState(false);
   const [produtoDetalhado, setProdutoDetalhado] = React.useState(null);
+
+  const produtosFiltrados = produtosEletronicos.filter(
+    (produto) =>
+      marcaSelecionada === "Todas" || produto.marca === marcaSelecionada
+  );
 
   const abrirModal = (produto) => {
     setProdutoDetalhado(produto);
@@ -82,6 +106,52 @@ const Products = () => {
     }
   };
 
+  const scrollContainerRef = React.useRef(null);
+  const [scrollInterval, setScrollInterval] = React.useState(null);
+  const [scrollDirection, setScrollDirection] = React.useState("right");
+
+  const scroll = () => {
+    if (scrollContainerRef.current) {
+      const scrollAmount = 1;
+
+      if (scrollDirection === "right") {
+        scrollContainerRef.current.scrollLeft += scrollAmount;
+
+        if (
+          scrollContainerRef.current.scrollLeft >=
+          scrollContainerRef.current.scrollWidth -
+            scrollContainerRef.current.clientWidth
+        ) {
+          setScrollDirection("left"); 
+        }
+      } else if (scrollDirection === "left") {
+        scrollContainerRef.current.scrollLeft -= scrollAmount;
+
+        if (scrollContainerRef.current.scrollLeft <= 0) {
+          setScrollDirection("right");
+        }
+      }
+    }
+  };
+
+  React.useEffect(() => {
+    const interval = setInterval(scroll, 30);
+    setScrollInterval(interval);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [scrollDirection]);
+
+  const handleMouseEnter = () => {
+    clearInterval(scrollInterval);
+  };
+
+  const handleMouseLeave = () => {
+    const interval = setInterval(scroll, 30);
+    setScrollInterval(interval);
+  };
+
   return (
     <div className="container mx-auto p-6">
       {/* Cabeçalho da Página */}
@@ -89,22 +159,57 @@ const Products = () => {
         Nossa Loja de Eletroferragens
       </h1>
 
+      {/* Filtro por Marcas */}
+      <section id="filtro-marcas" className="mb-8">
+        <h2 className="text-2xl font-semibold opacity-80 mb-4">
+          Filtrar por Marca
+        </h2>
+        <div
+          ref={scrollContainerRef}
+          className="flex overflow-x-auto pb-2 space-x-4"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          {marcas.map((marca) => (
+            <div
+              key={marca.nome}
+              onClick={() => setMarcaSelecionada(marca.nome)}
+              className={`cursor-pointer flex flex-col items-center p-4 rounded-lg transition transform hover:scale-105 ${
+                marcaSelecionada === marca.nome
+                  ? "bg-blue-600 shadow-lg"
+                  : "bg-gray-300 hover:bg-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700"
+              }`}
+              style={{ minWidth: "200px", flexShrink: 0 }}
+            >
+              <img
+                src={marca.imagem}
+                alt={marca.nome}
+                className="w-40 h-20 object-cover rounded-lg mb-2"
+              />
+              <span className="text-sm sm:text-base text-white">
+                {marca.nome}
+              </span>
+            </div>
+          ))}
+        </div>
+      </section>
+
       {/* Produtos Eletrônicos */}
       <section id="produtos-eletronicos" className="mb-12">
         <h2 className="text-2xl font-semibold opacity-80 mb-4">
           Produtos Eletrônicos
         </h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-          {produtosEletronicos.map((produto) => (
+          {produtosFiltrados.map((produto) => (
             <div
               key={produto.nome}
-              className="bg-white p-4 rounded-lg shadow-lg dark:bg-gray-800"
+              className="bg-white p-4 rounded-lg shadow-lg dark:bg-gray-800 transition-transform transform hover:scale-105"
             >
               <img
                 src={produto.imagem}
                 alt={produto.nome}
                 className="w-full h-48 object-cover rounded-lg mb-4 cursor-pointer"
-                onClick={() => abrirModal(produto)} // Clica na imagem para abrir o modal
+                onClick={() => abrirModal(produto)}
               />
               <h3 className="text-lg font-semibold text-blue-600">
                 {produto.nome}
@@ -112,9 +217,8 @@ const Products = () => {
               <p className="text-sm text-gray-600 dark:text-gray-300 mb-2">
                 {produto.descricao}
               </p>
-              <p className="font-semibold text-gray-800 dark:text-gray-300">{produto.preco}</p>
               <button
-                onClick={() => abrirModal(produto)} // Botão para abrir o modal
+                onClick={() => abrirModal(produto)}
                 className="mt-4 inline-block text-blue-500 hover:text-blue-700"
               >
                 Ver Detalhes
@@ -123,9 +227,8 @@ const Products = () => {
           ))}
         </div>
       </section>
-
       {/* Serviços de Montagem */}
-      <section id="produtos-montagem" className="mb-12">
+      <section id="servicos-montagem" className="mb-12">
         <h2 className="text-2xl font-semibold opacity-80 mb-4">
           Serviços de Montagem
         </h2>
@@ -133,13 +236,13 @@ const Products = () => {
           {servicosMontagem.map((servico) => (
             <div
               key={servico.nome}
-              className="bg-white p-4 rounded-lg shadow-lg dark:bg-gray-800"
+              className="bg-white p-4 rounded-lg shadow-lg dark:bg-gray-800 transition-transform transform hover:scale-105"
             >
               <img
                 src={servico.imagem}
                 alt={servico.nome}
                 className="w-full h-48 object-cover rounded-lg mb-4 cursor-pointer"
-                onClick={() => abrirModal(servico)} // Clica na imagem para abrir o modal
+                onClick={() => abrirModal(servico)}
               />
               <h3 className="text-lg font-semibold text-blue-600">
                 {servico.nome}
@@ -147,9 +250,8 @@ const Products = () => {
               <p className="text-sm text-gray-600 dark:text-gray-300 mb-2">
                 {servico.descricao}
               </p>
-              <p className="font-semibold text-gray-800 dark:text-gray-300">{servico.preco}</p>
               <button
-                onClick={() => abrirModal(servico)} // Botão para abrir o modal
+                onClick={() => abrirModal(servico)}
                 className="mt-4 inline-block text-blue-500 hover:text-blue-700"
               >
                 Ver Detalhes
@@ -168,13 +270,13 @@ const Products = () => {
           {consultoria.map((item) => (
             <div
               key={item.nome}
-              className="bg-white p-4 rounded-lg shadow-lg dark:bg-gray-800"
+              className="bg-white p-4 rounded-lg shadow-lg dark:bg-gray-800 transition-transform transform hover:scale-105"
             >
               <img
                 src={item.imagem}
                 alt={item.nome}
                 className="w-full h-48 object-cover rounded-lg mb-4 cursor-pointer"
-                onClick={() => abrirModal(item)} // Clica na imagem para abrir o modal
+                onClick={() => abrirModal(item)}
               />
               <h3 className="text-lg font-semibold text-blue-600">
                 {item.nome}
@@ -182,9 +284,8 @@ const Products = () => {
               <p className="text-sm text-gray-600 dark:text-gray-300 mb-2">
                 {item.descricao}
               </p>
-              <p className="font-semibold text-gray-800 dark:text-gray-300">{item.preco}</p>
               <button
-                onClick={() => abrirModal(item)} // Botão para abrir o modal
+                onClick={() => abrirModal(item)}
                 className="mt-4 inline-block text-blue-500 hover:text-blue-700"
               >
                 Ver Detalhes
@@ -219,11 +320,11 @@ const Products = () => {
             <p className="text-lg text-gray-800 dark:text-gray-300 mb-4">
               {produtoDetalhado.descricao}
             </p>
-            <p className="font-semibold text-gray-800 dark:text-gray-300">{produtoDetalhado.preco}</p>
             <div className="mt-4">
-              <button 
+              <button
                 onClick={fecharModal}
-                className="mt-4 px-4 py-2 bg-green-500 text-gray-900 rounded-md shadow-lg transition hover:bg-green-600 hover:shadow-xl">
+                className="mt-4 px-4 py-2 bg-green-500 text-gray-900 rounded-md shadow-lg transition hover:bg-green-600 hover:shadow-xl"
+              >
                 Fechar
               </button>
             </div>
