bb31a6f6807f344fd652e9ba13ac7b37475df869
yurannhassengo <yurannhassengo@gmail.com>
2024-11-27 06:38:43 -0800

    fixe builderror

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
 
 export default nextConfig;
